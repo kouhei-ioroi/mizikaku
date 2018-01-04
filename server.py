993d4c8ba6d2d1229fa9d3e375bf5260d229d2be
Kohe_Ioroi <ioroikouhei64@gmail.com>
2018-01-04 19:44:26 +0900

    短縮化
    ソースコードのゴミを除去

--- server.py
+++ server.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-import tornado.ioloop
-import tornado.web
-import tornado.escape
-import tornado.options
+import tornado.ioloop,tornado.web,tornado.escape,tornado.options
 from tornado.options import define, options
-import os
-import string
-import random
-import json
+import os,string,random,json
 url_list = json.load(open("urllist.json" , "r"))
 define("port", default=80, type=int)
 options.parse_command_line()
@@ -16,7 +10,6 @@ class Application(tornado.web.Application):
         handlers = [
             (r'/', MainHandler),
             (r'/s', ShortServiceHandler),
-            (r'/domainpool', DomainPoolHandler),
             (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "/static/favicon.ico"}),
             (r'/(robots.txt)', tornado.web.StaticFileHandler, {"path": "/static/robots.txt"}),
         ]
@@ -36,18 +29,16 @@ class MainHandler(tornado.web.RequestHandler):
         if url in url_list.values():
             url_list_key = list(url_list.keys())[list(url_list.values()).index(url)]
             self.render("form.html",url_list_math=len(url_list),url_short="http://www.mizikaku.ga/s?k="+url_list_key)
-            #self.write('<meta name="viewport" content="width=device-width,initial-scale=1.0"><p>Punycode(日本語URL):<a href="http://www.短く.ga/s?k='+url_list_key+'" target="_blank">http://www.短く.ga/s?k='+url_list_key+'<a></p><p>Unicode(通常URL):<a href="http://www.mizikaku.ga/s?k='+url_list_key+'" target="_blank">http://www.mizikaku.ga/s?k='+url_list_key+'<a></p><p>LINEで共有:<a href="http://line.me/R/msg/text/?http://www.mizikaku.ga/s?k='+url_list_key+'" target="_blank">LINEで共有<a></p>')
         else:
             url_list_key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(8)])
             url_list[url_list_key]=url
             json.dump(url_list,open("urllist.json" , "w"))
             self.render("form.html",url_list_math=len(url_list),url_short="http://www.mizikaku.ga/s?k="+url_list_key)
-            #self.write('<meta name="viewport" content="width=device-width,initial-scale=1.0"><p>Punycode(日本語URL):<a href="http://www.短く.ga/s?k='+url_list_key+'" target="_blank">http://www.短く.ga/s?k='+url_list_key+'<a></p><p>Unicode(通常URL):<a href="http://www.mizikaku.ga/s?k='+url_list_key+'" target="_blank">http://www.mizikaku.ga/s?k='+url_list_key+'<a></p><p>LINEで共有:<a href="http://line.me/R/msg/text/?http://www.mizikaku.ga/s?k='+url_list_key+'" target="_blank">LINEで共有<a></p>')
-class ShortServiceHandler(tornado.web.RequestHandler):
+            class ShortServiceHandler(tornado.web.RequestHandler):
     def get(self):
         key = self.get_argument('k', 'None')
         if key == "None":
-            self.write("Error:引数が不足しています。")
+            self.write("引数が不足しています。")
         else:
             if key in url_list:
                 try:
@@ -56,9 +47,6 @@ class ShortServiceHandler(tornado.web.RequestHandler):
                     self.write("転送中にエラーが発生しました。存在しないURLです。")
             else:
                 self.write("存在しない短縮URLです。")
-class DomainPoolHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('このドメインは<a href="https://twitter.com/Kohe_Ioroi/" target="_blank">Kohe_Ioroi</a>が管理しています。')
 def main():
     tornado.options.parse_command_line()
     app = Application()
