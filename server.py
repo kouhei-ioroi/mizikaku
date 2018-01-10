a7bee7fecda730f6903dbcf890e750395b75502d
Kohe_Ioroi <ioroikouhei64@gmail.com>
2018-01-11 01:04:04 +0900

    ダイレクトなディレクトリ指定に対応
    URLの短縮化に貢献

--- server.py
+++ server.py
@@ -12,6 +12,8 @@ class Application(tornado.web.Application):
             (r'/api', apiHandler),
             (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "/static/favicon.ico"}),
             (r'/(robots.txt)', tornado.web.StaticFileHandler, {"path": "/static/robots.txt"}),
+            (r'/........+', DirectShortServiceHandler),
+            (r'/[\S]+', ReturnNothingServiceHandler),
         ]
         settings = dict(
             static_path=os.path.join(os.path.dirname(__file__), "static"),
@@ -28,12 +30,22 @@ class MainHandler(tornado.web.RequestHandler):
         url = self.get_argument("element_1")
         if url in url_list.values():
             url_list_key = list(url_list.keys())[list(url_list.values()).index(url)]
-            self.render("form.html",url_list_math=len(url_list),url_short="http://www.mizikaku.ga/s?k="+url_list_key)
+            self.render("form.html",url_list_math=len(url_list),url_short="http://mzkk.ga/"+url_list_key)
         else:
             url_list_key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(8)])
             url_list[url_list_key]=url
             json.dump(url_list,open("urllist.json" , "w"))
-            self.render("form.html",url_list_math=len(url_list),url_short="http://www.mizikaku.ga/s?k="+url_list_key)
+            self.render("form.html",url_list_math=len(url_list),url_short="http://mzkk.ga/"+url_list_key)
+class DirectShortServiceHandler(tornado.web.RequestHandler):
+        def get(self):
+            key = self.request.path[1:]
+            if key in url_list:
+                try:
+                    self.redirect(url_list[key])
+                except:
+                    self.write("転送中にエラーが発生しました。存在しないURLです。")
+            else:
+                self.write("存在しない短縮URLです。")
 class ShortServiceHandler(tornado.web.RequestHandler):
     def get(self):
         key = self.get_argument('k', 'None')
@@ -51,16 +63,19 @@ class apiHandler(tornado.web.RequestHandler):
     def get(self):
         url = self.get_argument('url', 'None')
         if url == "None":
-            self.write("{'CODE':'500','RETURN':'NOTHING_URL_REQUEST'}")
+            self.write('{code":"500","id":"null","message":"No_Argument"}')
         else:
             if url in url_list.values():
                 url_list_key = list(url_list.keys())[list(url_list.values()).index(url)]
-                self.write("{'CODE':'200','RETURN':'"+url_list_key+"'}")
+                self.write('{"code":"200","id":'+url_list_key+'","message":"OK"}')
             else:
                 url_list_key = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(8)])
                 url_list[url_list_key]=url
                 json.dump(url_list,open("urllist.json" , "w"))
-                self.write("{'CODE':'200','RETURN':'"+url_list_key+"'}")
+                self.write('{"code":"200","id":'+url_list_key+'","message":"OK"}')
+class ReturnNothingServiceHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("存在しない短縮URLです。")
 def main():
     tornado.options.parse_command_line()
     Application().listen(options.port)
